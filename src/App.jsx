6513f25bc2ff2c98694eb4528c2e56b5b974290f
kevscon <kevscon@gmail.com>
2024-08-04 16:17:49 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import DataFetcher from './DataFetcher';
 const App = () => {
   return (
     <div>
-      <h1>My React App</h1>
+      <h1>My React Apples</h1>
       <DataFetcher />
     </div>
   );

eff7d0e5ddfea2ed122aa018eb67c691a25233cc
kevscon <kevscon@gmail.com>
2024-08-13 11:57:40 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ShapeInput from './ShapeInput';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Vite React App</h1>
+      <ShapeInput />
+    </div>
+  );
+};
+
+export default App;
